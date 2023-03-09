99eda7f28388a3223f1e6bfe069088fa5349e5d0
JeffreyManuel <125147857+JeffreyManuel@users.noreply.github.com>
2023-03-09 21:59:41 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -22,7 +22,7 @@ sudo mkdir /home/$USER/.config
 sudo mkdir /home/$USER/.fonts
 
 # Installing the dependencies and the window manager bspwm
-sudo yay -S nerd-fonts-complete-starship noto-fonts-emoji netctl brave variety feh ttf-font-awesome jq polybar redshift sddm nano vim sxhkd neofetch psmisc lxappearance papirus-icon-theme noto-fonts-emoji bspwm kitty polybar picom thunar nitrogen xorg unzip yad wget pulseaudio pavucontrol qt5-quickcontrols qt5-quickcontrols2 qt5-svg rofi lxpolkit-git ttf-font-awesome brave-git --noconfirm
+yay -S nerd-fonts-complete-starship noto-fonts-emoji netctl variety feh ttf-font-awesome jq polybar redshift sddm nano vim sxhkd neofetch psmisc lxappearance papirus-icon-theme noto-fonts-emoji bspwm kitty polybar picom thunar nitrogen xorg unzip yad wget pulseaudio pavucontrol qt5-quickcontrols qt5-quickcontrols2 qt5-svg rofi lxpolkit-git ttf-font-awesome brave-bin --noconfirm
 
 # Theming
 cd ~/jeff-arch || exit
