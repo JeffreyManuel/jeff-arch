58bb3feb9dbc8ac8df9b50eefcbe67a9cac1cc24
JeffreyManuel <jeffreymanuel9129@gmail.com>
2023-03-10 17:51:05 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # List available drives
 lsblk
 
 # Prompt user for input
-echo "Enter the drive to install Arch Linux on (e.g. /dev/sda):"
-read DRIVE
-echo "Enter the hostname for this system:"
-read HOSTNAME
-echo "Enter the username for the new user:"
-read USERNAME
-echo "Enter the password for the new user:"
-read -s USERPASS
-echo "Enter the root password:"
-read -s ROOTPASS
+read -rp "Enter the drive to install Arch Linux on (e.g. /dev/sda): " DRIVE
+read -rp "Enter the hostname for this system: " HOSTNAME
+read -rp "Enter the root password: " -s ROOTPASS
+read -rp "Enter the username for the new user: " USERNAME
+read -rp "Enter the password for the new user: " -s USERPASS
 
 # Verify UEFI boot mode
-if [[ ! -d /sys/firmware/efi/efivars ]]; then
-    echo "Error: UEFI boot mode not detected. Please boot in UEFI mode and run this script again."
+if [[ ! -d "/sys/firmware/efi/efivars" ]]; then
+    echo "Error: UEFI boot mode not detected. Please boot in UEFI mode and run this script again." >&2
     exit 1
 fi
 
@@ -25,19 +22,19 @@ fi
 timedatectl set-ntp true
 
 # Create partitions
-parted $DRIVE mklabel gpt
-parted $DRIVE mkpart primary fat32 1MiB 261MiB
-parted $DRIVE set 1 esp on
-parted $DRIVE mkpart primary ext4 261MiB 100%
+parted "${DRIVE}" mklabel gpt
+parted "${DRIVE}" mkpart primary fat32 1MiB 261MiB
+parted "${DRIVE}" set 1 esp on
+parted "${DRIVE}" mkpart primary ext4 261MiB 100%
 
 # Format partitions
-mkfs.fat -F32 ${DRIVE}1
-mkfs.ext4 ${DRIVE}2
+mkfs.fat -F32 "${DRIVE}1"
+mkfs.ext4 "${DRIVE}2"
 
 # Mount partitions
-mount ${DRIVE}2 /mnt
+mount "${DRIVE}2" /mnt
 mkdir -p /mnt/boot/efi
-mount ${DRIVE}1 /mnt/boot/efi
+mount "${DRIVE}1" /mnt/boot/efi
 
 # Install essential packages
 pacstrap /mnt base linux linux-firmware networkmanager git neofetch nano vim 
@@ -51,7 +48,7 @@ arch-chroot /mnt /bin/bash <<EOF
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 hwclock --systohc
 
-#Enable network manger
+# Enable network manager
 systemctl enable NetworkManager
 
 # Uncomment en_US.UTF-8 locale
@@ -59,32 +56,31 @@ sed -i '/^#en_US.UTF-8/s/^#//g' /etc/locale.gen
 locale-gen
 
 # Set hostname
-echo "$HOSTNAME" > /etc/hostname
+echo "${HOSTNAME}" > /etc/hostname
 
 # Generate hosts file
 cat >> /etc/hosts << END
 127.0.0.1   localhost
 ::1         localhost
-127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
+127.0.1.1   ${HOSTNAME}.localdomain ${HOSTNAME}
 END
 
 # Set root password
-echo "root:$ROOTPASS" | chpasswd
+echo "root:${ROOTPASS}" | chpasswd
 
 # Create user and set password
-useradd -m -G wheel -s /bin/bash $USERNAME
-echo "$USERNAME:$USERPASS" | chpasswd
+useradd -m -G wheel -s /bin/bash "${USERNAME}"
+echo "${USERNAME}:${USERPASS}" | chpasswd
 
 # Uncomment %wheel ALL=(ALL) ALL in sudoers file
 sed -i '/^# %wheel ALL=(ALL) ALL/s/^# //' /etc/sudoers
 
-#Installing the gui 
-cd /home/$USERNAME/
+# Install GUI
+cd /home/"${USERNAME}"
 git clone https://github.com/JeffreyManuel/jeff-arch
-chown $USERNAME:$USERNAME ./jeff-arch
+chown "${USERNAME}:${USERNAME}" ./jeff-arch
 cd jeff-arch
-su - $USERNAME -c /home/$USERNAME/jeff-arch/postinstall.sh
-
+su - "${USERNAME}" -c /home/"${USERNAME}
 
 # Install bootloader (systemd-boot)
 bootctl install
