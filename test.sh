244bb0ce4ff331ce3df284e0f0cd6a96b033979d
JeffreyManuel <jeffreymanuel9129@gmail.com>
2023-03-10 17:40:39 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -79,50 +79,10 @@ echo "$USERNAME:$USERPASS" | chpasswd
 sed -i '/^# %wheel ALL=(ALL) ALL/s/^# //' /etc/sudoers
 
 #Installing the gui 
+git clone https://github.com/JeffreyManuel/jeff-arch
+cd jeff-arch
+./postinstall.sh
 
-# Installing Basic Packages
-sudo pacman -Syu --needed --noconfirm archlinux-keyring pacman-contrib terminus-font base-devel rsync reflector
-sudo setfont ter-v22b
-iso=$(curl -4 ifconfig.co/country-iso)
-sudo timedatectl set-ntp true
-# Enable parellel Downloads
-sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-# Setting Mirrors for fast downloads 
-sudo reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
-
-# Installing yay aur helper
-git clone https://aur.archlinux.org/yay-git.git
-cd yay-git
-makepkg -si
-cd ..
-
-# Creating a few directories
-sudo mkdir /home/$USER/Pictures
-sudo mkdir /home/$USER/.config
-sudo mkdir /home/$USER/.fonts
-
-# Installing the dependencies and the window manager bspwm
-yay -S nerd-fonts-complete-starship noto-fonts-emoji netctl variety feh ttf-font-awesome jq polybar redshift sddm nano vim sxhkd neofetch psmisc lxappearance papirus-icon-theme noto-fonts-emoji bspwm kitty polybar picom thunar nitrogen xorg unzip yad wget pulseaudio pavucontrol qt5-quickcontrols qt5-quickcontrols2 qt5-svg rofi lxpolkit-git ttf-font-awesome brave-bin --noconfirm
-
-# Theming
-cd ~/jeff-arch || exit
-sudo cp -r bg.jpg /home/$USER/Pictures/bg.jpg
-sudo cp -r dotfonts/* /home/$USER/.fonts/
-sudo cp -r dotconfig/* /home/$USER/.config/
-
-git clone https://github.com/alvatip/Nordzy-cursors
-cd Nordzy-cursors 
-sudo bash install.sh
-sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-sudo mv dotfonts/fontawesome/*.otf /home/$USER/.fonts/
-sudo unzip FiraCode.zip -d /home/$USER/.fonts/
-sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-sudo unzip Meslo.zip -d /home/$USER/.fonts/
-sudo chown $USER:$USER /home/$USER/.fonts/*
-cd /usr/share/themes/ 
-sudo git clone https://github.com/EliverLara/Nordic.git
-sudo systemctl enable sddm
-sudo fc-cache -vf
 
 # Install bootloader (systemd-boot)
 bootctl install
